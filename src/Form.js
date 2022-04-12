7067f610f5fa259dd6ab60cf2e87518e2381deca
4ked <max.goeke0@gmail.com>
2022-04-11 22:50:26 -0400

    work and user ratings register

--- src/Form.js
+++ src/Form.js
@@ -3,13 +3,26 @@ import './App.css';
 function Form(props) {
   const name = props.name;
 
-  const nfts = {
+  const nft_works = {
     "27040 - Mutant Ape Yacht Club | OpenSea": "BBB",
     "MOAR #4368 - \"MOAR\" by Joan Cornella | OpenSea": "AA-",
     "---": "Not Generated"
   };
 
-  const work_rating = nfts[name];
+  const nft_owners = {
+    "27040 - Mutant Ape Yacht Club | OpenSea": "650",
+    "MOAR #4368 - \"MOAR\" by Joan Cornella | OpenSea": "720",
+    "---": "Not Generated"
+  };
+
+  let work_rating, user_rating;
+  if(name in nft_works) {
+    work_rating = nft_works[name];
+    user_rating = nft_owners[name];
+  } else {
+    work_rating = "Not Generated";
+    user_rating = "Not Generated";
+  }
 
   return (
     <div className="main">
@@ -25,7 +38,7 @@ function Form(props) {
          <li className="GassedRating">
            <div className="GassedRatingField">
              <span className="GassedRatingFieldTitle">NFT Rating</span>
-             <span className={`GassedRatingFieldStatus ${name in nfts ? 'Ok' : 'Error'}`}>
+             <span className={`GassedRatingFieldStatus ${name in nft_works ? 'Ok' : 'Error'}`}>
               {work_rating}
              </span>
            </div>
@@ -33,8 +46,8 @@ function Form(props) {
          <li className="GassedRating">
            <div className="GassedRatingField">
              <span className="GassedRatingFieldTitle">Owner Rating</span>
-             <span className="GassedRatingFieldStatus Error">
-              Not Generated
+             <span className={`GassedRatingFieldStatus ${name in nft_owners ? 'Ok' : 'Error'}`}>
+              {user_rating}
              </span>
            </div>
          </li>
